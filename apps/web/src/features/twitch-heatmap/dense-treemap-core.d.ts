6b809e402749aee8392dc7f3fc1eac8db7db6a8a
badjoke-lab <badjoke.lab@gmail.com>
2026-06-15 00:35:19 +0900

    remove unused dense treemap declaration shim

--- apps/web/src/features/twitch-heatmap/dense-treemap-core.d.ts
+++
@@ -1,9 +0,0 @@
-import type { HeatmapItem, TileLayout } from './model'
-
-export function buildDenseTreemap(
-  input: HeatmapItem[],
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-): TileLayout[]
