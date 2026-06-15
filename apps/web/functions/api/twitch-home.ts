cd4eb9522ab9282d99d0342c2f6a7824f37ace5c
badjoke-lab <badjoke.lab@gmail.com>
2026-06-15 20:55:14 +0900

    Add Twitch Home API endpoint

---
+++ apps/web/functions/api/twitch-home.ts
@@ -0,0 +1,9 @@
+import type { Env } from '../_db/env'
+import { buildProviderHomeResponse } from '../_home/model'
+
+export const onRequestGet: PagesFunction<Env> = async ({ env }) => buildProviderHomeResponse({
+  platform: 'twitch',
+  db: env.DB_TWITCH_HOT,
+  topLimit: 300,
+  staleAfterMinutes: 3,
+})
