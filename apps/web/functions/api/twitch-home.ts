b5a6a790645064034ee1dbfa7f065f03688b5fc2
badjoke-lab <badjoke.lab@gmail.com>
2026-06-17 23:58:18 +0900

    Keep Twitch Home runtime contract type-checked

--- apps/web/functions/api/twitch-home.ts
+++ apps/web/functions/api/twitch-home.ts
@@ -3,10 +3,12 @@ import { buildProviderHomeResponse } from '../_home/model'
 import { providerRuntime } from '../_provider-runtime'
 
 const runtime = providerRuntime('twitch')
+const topLimit: 300 = runtime.topLimit
+const staleAfterMinutes: 10 = runtime.staleAfterMinutes
 
 export const onRequestGet: PagesFunction<Env> = async ({ env }) => buildProviderHomeResponse({
   platform: 'twitch',
   db: env.DB_TWITCH_HOT,
-  topLimit: runtime.topLimit,
-  staleAfterMinutes: runtime.staleAfterMinutes,
+  topLimit: topLimit,
+  staleAfterMinutes: staleAfterMinutes,
 })
