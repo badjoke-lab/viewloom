a50c99d03a2610a14b00c72d6c316b1afda5a4cd
badjoke-lab <badjoke.lab@gmail.com>
2026-06-18 04:52:09 +0900

    Use shared Kick Home coverage contract

--- apps/web/functions/api/kick-home.ts
+++ apps/web/functions/api/kick-home.ts
@@ -1,14 +1,14 @@
 import type { Env } from '../_db/env'
-import { buildProviderHomeResponse } from '../_home/model'
+import { buildKickHomeResponse } from '../_home/kick'
 import { providerRuntime } from '../_provider-runtime'
 
 const runtime = providerRuntime('kick')
 const topLimit: 100 = runtime.topLimit
 const staleAfterMinutes: 10 = runtime.staleAfterMinutes
 
-export const onRequestGet: PagesFunction<Env> = async ({ env }) => buildProviderHomeResponse({
+export const onRequestGet: PagesFunction<Env> = async ({ env }) => buildKickHomeResponse({
   platform: 'kick',
   db: env.DB_KICK_HOT,
-  topLimit: topLimit,
-  staleAfterMinutes: staleAfterMinutes,
+  topLimit,
+  staleAfterMinutes,
 })
