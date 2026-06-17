480efd0cd73211df05b4dc9dbbae87cab615e810
badjoke-lab <badjoke.lab@gmail.com>
2026-06-17 23:45:23 +0900

    Use shared Twitch runtime settings in Home API

--- apps/web/functions/api/twitch-home.ts
+++ apps/web/functions/api/twitch-home.ts
@@ -1,9 +1,12 @@
 import type { Env } from '../_db/env'
 import { buildProviderHomeResponse } from '../_home/model'
+import { providerRuntime } from '../_provider-runtime'
+
+const runtime = providerRuntime('twitch')
 
 export const onRequestGet: PagesFunction<Env> = async ({ env }) => buildProviderHomeResponse({
   platform: 'twitch',
   db: env.DB_TWITCH_HOT,
-  topLimit: 300,
-  staleAfterMinutes: 10,
+  topLimit: runtime.topLimit,
+  staleAfterMinutes: runtime.staleAfterMinutes,
 })
