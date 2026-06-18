3f389ac8e9a5b6506908e0eb3b4514d39f4869cf
badjoke-lab <badjoke.lab@gmail.com>
2026-06-18 19:52:49 +0900

    Apply Twitch feature coverage enrichment

--- apps/web/functions/_middleware.ts
+++ apps/web/functions/_middleware.ts
@@ -1,5 +1,6 @@
 import type { Env } from './_db/env'
 import { enrichKickFeatureResponse } from './_kick-feature-coverage'
+import { enrichTwitchFeatureResponse } from './_twitch-feature-coverage'
 
 const KICK_FEATURE_ROUTES = new Set([
   '/api/kick-heatmap',
@@ -7,10 +8,17 @@ const KICK_FEATURE_ROUTES = new Set([
   '/api/kick-battle-lines',
 ])
 
+const TWITCH_FEATURE_ROUTES = new Set([
+  '/api/twitch-heatmap',
+  '/api/day-flow',
+  '/api/battle-lines',
+  '/api/history',
+])
+
 export const onRequest: PagesFunction<Env> = async ({ request, next, env }) => {
   const response = await next()
   const pathname = new URL(request.url).pathname.replace(/\/$/, '')
-  return KICK_FEATURE_ROUTES.has(pathname)
-    ? enrichKickFeatureResponse(env, response)
-    : response
+  if (KICK_FEATURE_ROUTES.has(pathname)) return enrichKickFeatureResponse(env, response)
+  if (TWITCH_FEATURE_ROUTES.has(pathname)) return enrichTwitchFeatureResponse(env, response)
+  return response
 }
