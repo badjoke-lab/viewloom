5b35d6857514106f467ee1a1d283bb4ddfe87eb1
badjoke-lab <badjoke.lab@gmail.com>
2026-08-26 23:56:45 +0900

    Add Twitch Stream Map Batch D canonical exclusion

---
+++ apps/web/functions/api/twitch-stream-map-reviewed-evidence-batch-d.mjs
@@ -0,0 +1,11 @@
+export const TWITCH_REVIEWED_LOCATION_BATCH_D = [
+  {
+    streamerLogin: 'japanese_restream',
+    entityKind: 'event_broadcast',
+    classificationReferences: [
+      'https://note.com/keynnaka/n/nfdbb9db1f8d3',
+      'https://www.twitch.tv/japanese_restream',
+    ],
+    evidences: [],
+  },
+]
