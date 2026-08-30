8bbd753398a911bb5d96c01332e7a6281ed362cf
badjoke-lab <badjoke.lab@gmail.com>
2026-08-30 12:02:06 +0900

    Add Country Batch G canonical records

---
+++ apps/web/functions/api/twitch-stream-map-reviewed-evidence-batch-g.mjs
@@ -0,0 +1,19 @@
+export const TWITCH_REVIEWED_LOCATION_BATCH_G = [
+  {
+    streamerLogin: 'bobross',
+    entityKind: 'event_broadcast',
+    classificationReferences: [
+      'https://blog.twitch.tv/en/2015/10/28/introducing-twitch-creative-fbfe23b4a114/',
+      'https://blog.twitch.tv/en/2016/10/28/for-the-love-of-bob-6b3b73957938/',
+    ],
+    evidences: [],
+  },
+  {
+    streamerLogin: 'zerator_247',
+    entityKind: 'event_broadcast',
+    classificationReferences: [
+      'https://bsky.app/profile/zerator.bsky.social/post/3mt6wvzvfxc2c',
+    ],
+    evidences: [],
+  },
+]
